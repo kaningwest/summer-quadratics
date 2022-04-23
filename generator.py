64550949ebe296ce2e3e88828e127dd8c432b930
ning nong <88621543+kaningwest@users.noreply.github.com>
2022-04-23 12:59:48 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,2 +1,4 @@
 import streamlit as st
 import numpy as np
+st.sidebar.title("Problem Types")
+options = st.sidebar.selectbox("", ("Introduction","Platform Info","Years and Ratings", "Genre Studies", "Conclusion"))
